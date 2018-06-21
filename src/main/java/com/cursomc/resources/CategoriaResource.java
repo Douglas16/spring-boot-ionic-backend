6d249618dca7d1631fe47d54d16cfc67f6c9c592
Douglas16 <douglas.salvador@gmail.com>
2018-06-21 17:36:58 -0300

    Repositorio_CategoriaBy_Id

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -3,29 +3,30 @@ package com.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursomc.domain.Categoria;
+import com.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	@Autowired
+	private CategoriaService service;
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@RequestMapping(value="/{id}",method=RequestMethod.GET) //find id pois recebe um id na url
+	public ResponseEntity<?> find(@PathVariable Integer id) { //anotacao @pathvariable...
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+				
 		
-		//CRIAR DOIS OBJETOS DO TIPO CATERIA PARA TESTAR O REST
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		//lista nao pode ser instanciado pois é interface, acha uma classe que usa essa interface
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
 		
-		return lista;
 	}
 
 }
