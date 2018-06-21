24013a27af402e6fbb4a81f34465637bbcd41552
Douglas16 <douglas.salvador@gmail.com>
2018-06-21 15:43:38 -0300

    Classe Categoria_teste

---
+++ src/main/java/com/cursomc/domain/Categoria.java
@@ -0,0 +1,87 @@
+package com.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable //convertidos para uma sequencia de byte, rede, arquivos
+//exigencia do java
+{
+	
+	//ATRIBUTOS BASICOS
+	
+	private static final long serialVersionUID = 1L; //serializer padrao
+	private Integer id;
+	private String nome;
+
+
+
+//construtor vazio -> Instancio o objeto sem jogar nada para os atributos
+
+public Categoria() {
+	}
+
+//CONSTRUTOR COM OS ATRIBUTOS PARA ALIMENTAR OS DADOS DO CONSTRUTOR
+//GERADOR CODIGO SOURCE GENERATOR -> COSNTRUTOR USER FIELDS
+
+
+public Categoria(Integer id, String nome) {
+	super();
+	this.id = id;
+	this.nome = nome;
+}
+
+//GETTERS E SETTERS POR PADRAO --> PADRAO PRIOVADO PARA NAO ACESSAR POR OUTRAS CLASSE
+//USA ENTÃO O GETTES E SETTERS METODOS DE ACESSOS. SOURCE --> 
+
+public Integer getId() {
+	return id;
+}
+
+public void setId(Integer id) {
+	this.id = id;
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	return result;
+}
+
+
+
+
+
+//HASCODE EQUALS --> COMPARADO PELO CONTEUDO METODOS DE COMPARAÇÃO POR VALOR
+
+//SOURCE HASRCOD
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Categoria other = (Categoria) obj;
+	if (id == null) {
+		if (other.id != null)
+			return false;
+	} else if (!id.equals(other.id))
+		return false;
+	return true;
+
+}
+
+
+
+}
--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,31 @@
 package com.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "Rest está funcionando!";
+		//CRIAR DOIS OBJETOS DO TIPO CATERIA PARA TESTAR O REST
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		//lista nao pode ser instanciado pois é interface, acha uma classe que usa essa interface
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
