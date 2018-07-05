36011c0b3312f70aa73613c55a1ac4fd9ab201d7
Douglas16 <douglas.salvador@gmail.com>
2018-07-05 12:01:19 -0300

    validção cpf e cnpj

--- src/main/java/com/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/cursomc/repositories/ClienteRepository.java
@@ -2,11 +2,16 @@ package com.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cursomc.domain.Cliente;
 
 //BUSCA NO CAMPO COM ESSA INTERFACE E @IDENT
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true) //mais rapido e diminio o lock de banco
+	Cliente findByEmail(String email);
 
 }
+
--- src/main/java/com/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cursomc.domain.Cliente;
 import com.cursomc.domain.enums.TipoCliente;
 import com.cursomc.dto.ClienteNewDTO;
+import com.cursomc.repositories.ClienteRepository;
 import com.cursomc.resources.exception.FieldMessage;
 import com.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -40,6 +47,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		
 		// inclua os testes aqui, inserindo erros na lista
+		
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
 		
 		
 		for (FieldMessage e : list) {
